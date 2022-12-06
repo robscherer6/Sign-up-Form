16d98083cbe28c35dfff43986402761336d262f4
Rob Scherer <robscherer6@gmail.com>
2022-12-05 20:16:02 -0500

    creating equality check for passwords

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const password = document.getElementById('password');
-password.onfocus = function () {
-  document.getElementById('message').style.display = 'block';
-}
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('hi')
+})
 
-password.onblur = function () {
-  document.getElementById('message').style.display = 'none';
-}
+let p1 = document.querySelector('#password').value;
+let p2 = document.querySelector('#confirmPassword').value;
+
+
+
+// const password = document.getElementById('password');
+// password.onfocus = function () {
+//   document.getElementById('message').style.display = 'block';
+// }
+
+// password.onblur = function () {
+//   document.getElementById('message').style.display = 'none';
+// }
 
 
 const button = document.querySelector('.btn');
@@ -13,4 +22,9 @@ const button = document.querySelector('.btn');
 button.addEventListener('click', (e) => {
   console.log('The button has been clicked!')
   button.innerHTML = 'Clicked!';
+  if (p1 !== p2) {
+    alert('passwords do not match!')
+  } else {
+    console.log('passwords match!')
+  }
 })
