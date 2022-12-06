d539d4672d205c5e12990fa21784054531350f5e
Rob Scherer <robscherer6@gmail.com>
2022-12-05 19:14:37 -0500

    added :focus/:valid/:invalid css rules to inputs

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+const password = document.getElementById('password');
+password.onfocus = function () {
+  document.getElementById('message').style.display = 'block';
+}
+
+password.onblur = function () {
+  document.getElementById('message').style.display = 'none';
+}
+
+
 const button = document.querySelector('.btn');
 
 button.addEventListener('click', (e) => {
