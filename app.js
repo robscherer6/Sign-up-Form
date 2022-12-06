84707f82587c1f32709a08a06067ffc5604265e5
Rob Scherer <robscherer6@gmail.com>
2022-12-06 05:50:00 -0500

    added password equality check and uppercase labels

--- app.js
+++ app.js
@@ -1,30 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  console.log('hi')
-})
+let password = document.querySelector('#password').value;
+let confirmPassword = document.querySelector('#confirmPassword').value;
 
-let p1 = document.querySelector('#password').value;
-let p2 = document.querySelector('#confirmPassword').value;
-
-
-
-// const password = document.getElementById('password');
-// password.onfocus = function () {
-//   document.getElementById('message').style.display = 'block';
-// }
-
-// password.onblur = function () {
-//   document.getElementById('message').style.display = 'none';
-// }
-
+let error = document.querySelector('#error');
 
 const button = document.querySelector('.btn');
 
 button.addEventListener('click', (e) => {
   console.log('The button has been clicked!')
   button.innerHTML = 'Clicked!';
-  if (p1 !== p2) {
-    alert('passwords do not match!')
-  } else {
-    console.log('passwords match!')
+  if (password === confirmPassword) {
+    error.innerHTML = ''
   }
 })
